922b882eb8a7344d9d170a5e0812dddcd2d82409
cm718 <cm718@icloud.com>
2019-01-26 14:11:45 -0600

    added some seed value

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('double bacon cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('single bacon cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mac and cheese', false);
