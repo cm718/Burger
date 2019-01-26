a6b4daa7d8c52b1c491de312a16ca0440ce2c8fc
cm718 <cm718@icloud.com>
2019-01-25 22:30:57 -0600

    required in the orm doc and exported the burger object

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+
+// Require in the ORM
+const orm = require('../config/orm');
+
+// Use the functions created in ORM
+const burger = {};
+
+// Export the functions that were created in ORM
+module.exports = burger;
