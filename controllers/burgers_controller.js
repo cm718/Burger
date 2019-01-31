bd581d0190bdc5275a91d1e1d6d5a472ead9014e
cm718 <cm718@icloud.com>
2019-01-31 17:41:59 -0600

    Set up the routes in the controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,3 @@
-
 // Import express and the burger.js object
 const express = require('express');
 const burger = require('../models/burger');
@@ -6,12 +5,36 @@ const burger = require('../models/burger');
 const router = express.Router();
 
 // Create the routes
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+    res.redirect("/burgers");
+});
 
-router.post("/", (req, res) => {});
+router.get("/", (req, res) => {
+    burger.all(function (burgerData) {
+        res.render("index", {
+            burger_data: burgerData
+        });
+    });
+});
 
-router.put("/", (req, res) => {});
-
+router.post("/burgers/create", function(req, res) {
+    burger.create(
+      req.body.burger_name, 
+      function(result) {
+        console.log(result);
+      // Send back the ID of the new quote
+      res.redirect("/");
+    });
+  });
+  
+  router.put("/burgers/:id", function(req, res) {
+    burger.update(
+      req.params.id,
+      function(result) {
+      console.log(result)
+      res.sendStatus(200);
+    });
+  });
 
 // export the router
 module.exports = router;
