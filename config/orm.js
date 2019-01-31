641d253335910d7e3993f40f7e5cca96a87d2e35
cm718 <cm718@icloud.com>
2019-01-31 17:20:25 -0600

    wrote the functions for my Create, Retrieve, and Update

--- config/orm.js
+++ config/orm.js
@@ -2,14 +2,45 @@
 // Import (require) connection.js into orm.js
 const connection = require('../config/connection');
 
-// create the methods that will execute the necessary MySQL commands in the controllers
+// create the methods that will execute the necessary MySQL commands in the controller
 const orm = {
-    selectAll: function(){},
+    // Method to receive all the burgers in the table
+    selectAll: function(tableInput, cb) {
+        const query = `SELECT * FROM ${tableInput};`;
+        connection.query(query, function(err, result){
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
-    insertOne: function(){},
+    // Method to add a new burger to the table
+    insertOne: function(burger_name, cb){
+        const query = `INSERT INTO burgers VALUES(${burger_name})`;
+        connection.query(query, function(err, result){
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
-    updateOne: function(){}
+    // Method to update a burger's data based on the id given
+    updateOne: function(id, cb){
+        const burger = req.body.burger_name;
+        const devoured = req.body.devoured;
+        const id = req.params.id;
+        const query = `UPDATE burgers SET burger_name = ?, devoured = ? WHERE id = ?`;
+        
+        connection.query(query, function(err, result){
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 };
 
-// Export the ORM object in module.exports.
+// Export the ORM object to go to burger.js in the models folder.
 module.exports = orm;
