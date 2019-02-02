f10c97c642b9257da91893813496d467d4320a3c
cm718 <cm718@icloud.com>
2019-02-02 11:10:30 -0600

    trying to fix / route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,35 +6,39 @@ const router = express.Router();
 
 // Create the routes
 router.get("/", (req, res) => {
-    res.redirect("/burgers");
+  burger.all(function (burgerData) {
+    res.render("index", {
+      burger_data: burgerData
+    });
+  });
 });
 
 router.get("/", (req, res) => {
-    burger.all(function (burgerData) {
-        res.render("index", {
-            burger_data: burgerData
-        });
+  burger.all(function (burgerData) {
+    res.render("index", {
+      burger_data: burgerData
     });
+  });
 });
 
-router.post("/burgers/create", function(req, res) {
-    burger.create(
-      req.body.burger_name, 
-      function(result) {
-        console.log(result);
+router.post("/burgers/create", function (req, res) {
+  burger.create(
+    req.body.burger_name,
+    function (result) {
+      console.log(result);
       // Send back the ID of the new quote
       res.redirect("/");
     });
-  });
-  
-  router.put("/burgers/:id", function(req, res) {
-    burger.update(
-      req.params.id,
-      function(result) {
+});
+
+router.put("/burgers/:id", function (req, res) {
+  burger.update(
+    req.params.id,
+    function (result) {
       console.log(result)
       res.sendStatus(200);
     });
-  });
+});
 
 // export the router
 module.exports = router;
