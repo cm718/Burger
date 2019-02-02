58141265132e8853a863efbe3df6c0d360f54bf4
cm718 <cm718@icloud.com>
2019-02-02 13:44:16 -0600

    updated

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,44 @@
-$(document).ready(() => {
+import {
+    get
+} from "http";
 
+$(document).ready(function () {
+
+    $("#create").on("submit", function (event) {
+        event.preventDefault();
+
+        let newBurger = {
+            burger_name: $(this).val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("new burger added");
+                location.reload();
+            }
+        );
+    });
+
+    $("#devour").on("click", function (event) {
+        let id = $(this).data("id");
+        console.log($(this).attr("data-id"));
+        let devoured = 1;
+        console.log(id, devoured);
+
+        $.ajax("/api/burgers/", id, {
+            type: "PUT",
+            data: devoured
+        }).then(
+            function () {
+                console.log("changed devoured to " + devoured);
+                location.reload();
+            }
+        );
+    });
 
 
 });
